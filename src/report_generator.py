258315e2f8927033350c6614208176cfc365040c
George Stavrinos <gstavrinos@iit.demokritos.gr>
2017-04-27 15:04:43 +0200

    Fully functional version.

--- src/report_generator.py
+++ src/report_generator.py
@@ -5,6 +5,7 @@ import rospkg
 import smtplib
 import numpy as np
 from datetime import datetime
+from datetime import timedelta
 from std_msgs.msg import Int32
 from kobuki_msgs.msg import Sound
 from email.mime.text import MIMEText
@@ -34,19 +35,19 @@ def generateReport(msg):
     if not os.path.exists(os.path.expanduser(radio_logs)):
         os.makedirs(os.path.expanduser(radio_logs))
     else:
-        for i in os.listdir(radio_logs):
+        for i in os.listdir(os.path.expanduser(radio_logs)):
             if 'report' in i:
                 i = i.replace('report','')
                 i = i.replace('.csv','')
-                if int(i) > rmax:
-                    rmax = int(i)
-    with open(radio_logs+'/'+'report'+str(rmax)+'.csv','w+') as report_file:
+                if int(i) >= rmax:
+                    rmax = int(i) + 1
+    with open(os.path.expanduser(radio_logs)+'/'+'report'+str(rmax)+'.csv','w+') as report_file:
         path = rospack.get_path('motion_analysis_wrapper')+'/logs/'
         files = []
         found_file = False
         for i in os.listdir(path):
             for d in range(2, -1, -1):
-                if os.path.isfile(os.path.join(path,i)) and 'official_log_bed_'+(datetime.date.today()-datetime.timedelta(d)).strftime("%d-%m-%Y") in i:
+                if os.path.isfile(os.path.join(path,i)) and 'official_log_bed_'+(datetime.today()-timedelta(d)).strftime("%d-%m-%Y") in i:
                     found_file = True
                     files.append(i)
                     files_to_move.append(path+i)
@@ -60,9 +61,9 @@ def generateReport(msg):
             for f in files:
                 content = np.genfromtxt(path+f, delimiter=',')
                 content = content[1]
-                report_file.write(annotations1[counter]+','+annotations2[counter]+','+repetition+','+str(content)+'\n')
+                report_file.write(annotations1[counter]+','+annotations2[counter]+','+repetition+','+str(content[0])+','+str(content[1])+'\n')
                 counter += 1
-                if counter == len(annotations):
+                if counter == len(annotations1):
                     counter = 0
                     repetition = 'b'
 
@@ -71,7 +72,7 @@ def generateReport(msg):
 
         for i in os.listdir(path):
             for d in range(2, -1, -1):
-                if os.path.isfile(os.path.join(path,i)) and 'official_log_pill_'+(datetime.date.today()-datetime.timedelta(d)).strftime("%d-%m-%Y") in i:
+                if os.path.isfile(os.path.join(path,i)) and 'official_log_pill_'+(datetime.today()-timedelta(d)).strftime("%d-%m-%Y") in i:
                     found_file = True
                     files.append(i)
                     files_to_move.append(path+i)
@@ -81,12 +82,13 @@ def generateReport(msg):
         annotations1 = ['10', '22', '44', '63']
         annotations2 = ['V5', 'V7', 'V18', 'V28']
         if found_file:
+            files.sort()
             for f in files:
-                content = np.genfromtxt(path+f, delimiter=',')
+                content = np.genfromtxt(path+f, delimiter=',',dtype=None)
                 content = content[1]
-                report_file.write(annotations1[counter]+','+annotations2[counter]+','+repetition+','+str(content)+'\n')
+                report_file.write(annotations1[counter]+','+annotations2[counter]+','+repetition+','+str(content[0])+','+str(content[1])+'\n')
                 counter += 1
-                if counter == len(annotations):
+                if counter == len(annotations1):
                     counter = 0
                     repetition = 'b'
         files = []
@@ -95,16 +97,17 @@ def generateReport(msg):
         path = rospack.get_path('hpr_wrapper')+'/logs/'
         for i in os.listdir(path):
             for d in range(2, -1, -1):
-                if os.path.isfile(os.path.join(path,i)) and 'official_log_walk_'+(datetime.date.today()-datetime.timedelta(d)).strftime("%d-%m-%Y") in i:
+                if os.path.isfile(os.path.join(path,i)) and 'official_log_walk_'+(datetime.today()-timedelta(d)).strftime("%d-%m-%Y") in i:
                     found_file = True
                     files.append(i)
                     files_to_move.append(path+i)
 
         counter = 0
         repetition = 'a'
-        annotations1 = ['6', '24', '30', '50', '69', '75']
-        annotations2 = ['V12', 'V8', 'V11', 'V21', 'V31', 'V34']
+        annotations1 = ['12', '24', '30', '50', '65', '69', '75']
+        annotations2 = ['V6', 'V8', 'V11', 'V21', 'V29', 'V31', 'V34']
         if found_file:
+            files.sort()
             for f in files:
                 content = []
                 filtered = []
@@ -115,10 +118,9 @@ def generateReport(msg):
                 filtered.sort(key=lambda x: x[2])
                 filtered = filtered[len(filtered)/2]
                 report_file.write(annotations1[counter]+','+annotations2[counter]+','+repetition+',')
-                report_file.write(str(filtered[1])+',')
                 report_file.write(str(filtered[2])+'\n')
                 counter += 1
-                if counter == len(annotations):
+                if counter == len(annotations1):
                     counter = 0
                     repetition = 'b'
 
@@ -128,7 +130,7 @@ def generateReport(msg):
         path = rospack.get_path('ros_visual_wrapper')+'/logs/'
         for i in os.listdir(path):
             for d in range(2, -1, -1):
-                if os.path.isfile(os.path.join(path,i)) and 'official_log_chair_'+(datetime.date.today()-datetime.timedelta(d)).strftime("%d-%m-%Y") in i:
+                if os.path.isfile(os.path.join(path,i)) and 'official_log_chair_'+(datetime.today()-timedelta(d)).strftime("%d-%m-%Y") in i:
                     found_file = True
                     files.append(i)
                     files_to_move.append(path+i)
@@ -138,20 +140,22 @@ def generateReport(msg):
         annotations1 = ['8', '28', '34', '48', '61']
         annotations2 = ['V4', 'V10', 'V13', 'V20', 'V27']
         if found_file:
+            files.sort()
             for f in files:
                 content = np.genfromtxt(path+f, delimiter=',')
                 content = content[1:-1]
                 content.sort()
                 content = content[len(content)/2]
-                nf.write(str(content)+"\n")
+                report_file.write(annotations1[counter]+','+annotations2[counter]+','+repetition+',')
+                report_file.write(str(content)+"\n")
                 counter += 1
-                if counter == len(annotations):
+                if counter == len(annotations1):
                     counter = 0
                     repetition = 'b'
 
-    with open(radio_logs+'/report'+rmax+'.csv', 'rb') as myfile:
+    with open(os.path.expanduser(radio_logs)+'/report'+str(rmax)+'.csv', 'rb') as myfile:
         part = MIMEApplication(myfile.read(), Name=f)
-        part['Content-Disposition'] = 'attachment; filename="%s"' % f
+        part['Content-Disposition'] = 'attachment; filename=%s' % 'report'+str(rmax)+'.csv'
         msg.attach(part)
     msg.attach(MIMEText(body, 'plain'))
 
@@ -162,7 +166,7 @@ def generateReport(msg):
     server.sendmail(fromaddr, toaddr, text)
     server.quit()
     for f in files_to_move:
-        os.rename(f,radio_logs+'/'+f.rpartition('/')[-1])
+        #os.rename(os.path.expanduser(f),os.path.expanduser(radio_logs)+'/'+f.rpartition('/')[-1])
         print 'Moved',f
     sound_msg = Sound()
     sound_msg.value = 0
